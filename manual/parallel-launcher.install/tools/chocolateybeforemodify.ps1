71b1b5aa916f676cb704f048e8df036337198204
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-17 03:25:07 +0200

    Comment on a script

--- manual/parallel-launcher.install/tools/chocolateybeforemodify.ps1
+++ manual/parallel-launcher.install/tools/chocolateybeforemodify.ps1
@@ -1,16 +1,18 @@
-$ErrorActionPreference = 'Stop'
+# $ErrorActionPreference = 'Stop'
 
-try {
-  $programRunning = Get-Process "parallel-launcher"
-} catch {
-  echo "program is NOT running"
-  echo $programRunning
-  exit 0
-}
+# Doesn't seem like what I am trying to do is possible at the
+# https://github.com/chocolatey/choco/issues/1731
+# try {
+#   $programRunning = Get-Process "parallel-launcher"
+# } catch {
+#   echo "program is NOT running"
+#   echo $programRunning
+#   exit 0
+# }
 
-try {
-  throw "Parallel Launcher is running. Stop Parallel Launcher and try again."
-} catch {
-  Write-Error $_.Exception.Message
-  exit 1
-}
+# try {
+#   throw "Parallel Launcher is running. Stop Parallel Launcher and try again."
+# } catch {
+#   Write-Error $_.Exception.Message
+#   exit 1
+# }
