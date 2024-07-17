a1827742d303790286ad664e130ae75681d18398
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-17 02:45:48 +0200

    Trying to get these to work.

--- manual/parallel-launcher.install/tools/chocolateybeforemodify.ps1
+++ manual/parallel-launcher.install/tools/chocolateybeforemodify.ps1
@@ -1,2 +1,16 @@
-$ErrorActionPreference = 'SilentlyContinue'
-Stop-Process -Name "parallel-launcher" 2> $null
+$ErrorActionPreference = 'Stop'
+
+try {
+  $programRunning = Get-Process "parallel-launcher"
+} catch {
+  echo "program is NOT running"
+  echo $programRunning
+  exit 0
+}
+
+try {
+  throw "Parallel Launcher is running. Stop Parallel Launcher and try again."
+} catch {
+  Write-Error $_.Exception.Message
+  exit 1
+}
