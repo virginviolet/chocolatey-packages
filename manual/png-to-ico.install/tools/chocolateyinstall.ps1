df0c077a9592123a0ca8de79434085123212a29b
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-19 21:42:21 +0200

    Update chocolateyinstall.ps1
    Removed template comments.

--- manual/png-to-ico.install/tools/chocolateyinstall.ps1
+++ manual/png-to-ico.install/tools/chocolateyinstall.ps1
@@ -1,7 +1,3 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'png_to_ico_setup.exe'
