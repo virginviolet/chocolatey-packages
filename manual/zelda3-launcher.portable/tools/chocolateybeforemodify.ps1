3c59ec1cdfe4987c88a1d9f42c2c73069a9ae3d1
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-23 14:37:17 +0200

    Fixed chocolateybeforemodify

--- manual/zelda3-launcher.portable/tools/chocolateybeforemodify.ps1
+++ manual/zelda3-launcher.portable/tools/chocolateybeforemodify.ps1
@@ -1,27 +1,9 @@
 ﻿# get zelda3 process
 try {
-    $zelda3 = Get-Process "zelda3"
-    if ($zelda3) {
-        # try gracefully first
-        $zelda3.CloseMainThread()
-        # kill after five seconds
-        Sleep 5
-        if (!$zelda3.started) {
-            $zelda3 | Stop-Process -Force
-        }
-    }
+    Stop-Process -Name "Zelda 3 Launcher" -F
 } catch {}
 
 # get launcher process
 try {
-    $zelda3launcher = Get-Process "Zelda 3 Launcher"
-        if ($zelda3launcher) {
-        # try to close gracefully first
-        $zelda3launcher.CloseMainThread()
-        # kill after five seconds
-        Sleep 5
-        if (!$zelda3launcher.started) {
-            $zelda3launcher | Stop-Process -Force
-        }
-    }
+    Stop-Process -Name "Zelda 3 Launcher" -F
 } catch {}
