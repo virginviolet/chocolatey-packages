91e71c559a752f5d8f37e10d25af9f28518b6683
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-23 14:44:08 +0200

    Cleaned up

--- manual/zelda3-launcher.portable/tools/chocolateyinstall.ps1
+++ manual/zelda3-launcher.portable/tools/chocolateyinstall.ps1
@@ -1,10 +1,4 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:/path/to/thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^/s*#"} | % {$_ -replace '(^.*?)/s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-# 1. See the _TODO.md that is generated top level and read through that
-# 2. Follow the documentation below to learn how to create a package for the package type you are creating.
-$ErrorActionPreference = 'Stop' # stop on all errors
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipArchive = Join-Path $toolsDir -ChildPath 'Zelda_3_Launcher_v1.3.6.0.zip'
 $unzipDir   = "C:/Games/Zelda 3 Launcher"
