620d3a66253f0eacf3c5a0c429b9c05817a0a9d9
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-23 13:14:55 +0200

    Replaced try catch statements with if statements

--- manual/zelda3-launcher.portable/tools/chocolateybeforemodify.ps1
+++ manual/zelda3-launcher.portable/tools/chocolateybeforemodify.ps1
@@ -1,9 +1,27 @@
-﻿# This runs before upgrade or uninstall.
-# Use this file to do things like stop services prior to upgrade or uninstall.
-# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
-#  need to uninstall an MSI prior to upgrade, put the functionality in this
-#  file without calling the uninstall script. Make it idempotent in the
-#  uninstall script so that it doesn't fail when it is already uninstalled.
-# NOTE: For upgrades - like the uninstall script, this script always runs from
-#  the currently installed version, not from the new upgraded package version.
+﻿# get zelda3 process
+try {
+    $zelda3 = Get-Process "zelda3"
+    if ($zelda3) {
+        # try gracefully first
+        $zelda3.CloseMainThread()
+        # kill after five seconds
+        Sleep 5
+        if (!$zelda3.started) {
+            $zelda3 | Stop-Process -Force
+        }
+    }
+} catch {}
 
+# get launcher process
+try {
+    $zelda3launcher = Get-Process "Zelda 3 Launcher"
+        if ($zelda3launcher) {
+        # try to close gracefully first
+        $zelda3launcher.CloseMainThread()
+        # kill after five seconds
+        Sleep 5
+        if (!$zelda3launcher.started) {
+            $zelda3launcher | Stop-Process -Force
+        }
+    }
+} catch {}
--- manual/zelda3-launcher.portable/tools/chocolateyuninstall.ps1
+++ manual/zelda3-launcher.portable/tools/chocolateyuninstall.ps1
@@ -1,13 +1,14 @@
 ﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+#   $f='c:/path/to/thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^/s*#"} | % {$_ -replace '(^.*?)/s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 
 $ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipArchive = Join-Path $toolsDir -ChildPath 'Zelda_3_Launcher_v1.3.6.0.zip'
-$unzipDir   = "C:\Games\Zelda 3 Launcher"
+$unzipDir   = "C:/Games/Zelda 3 Launcher"
 $zelda3Dir  = Join-Path $unzipDir 'zelda3'
 $saveDir    = Join-Path $zelda3Dir 'Saves'
+$saveDirRef = Join-Path $saveDir 'ref'
 $config     = Join-Path $zelda3Dir 'zelda.ini'
 $tempZelda3 = Join-Path $env:TEMP 'zelda3'
 # $tempZelda3 = Join-Path $toolsDir 'zelda3'
@@ -16,61 +17,75 @@ Write-Warning 'Zelda 3 will be removed if found in the Zelda 3 Launcher director
 Write-Warning 'Settings and save files will not be removed.'
 # Start-Sleep 10
 
-# Move saves and confg to temporary location (because Powershell is hell), if they exist
-New-Item -ItemType Directory -Path $tempZelda3
-Write-Warning "Created folder."
-# Start-Sleep 5
+# Remove reference saves files directory if empty (not likely though)
+$Exists = Test-Path -Path $saveDirRef
+$Empty = -Not $(Test-Path -Path $saveDirRef/*)
+if ($Empty){
+    Remove-Item $saveDirRef
+}
 
-# Remove reference save files if they exist
-try {Remove-Item $(Join-Path $saveDir ref) -Force -Recurse} catch {}
-
-# If we cannot move, the folders and file likely do not exist, which is fine.
-try {Move-Item -Path $saveDir -Destination $tempZelda3} catch {Write-Warning "Could not move saves."}
-try {Move-Item -Path $config -Destination $tempZelda3} catch {Write-Warning "Could not move config."}
-
-# Remove everything in the launcher directory, including zelda3 (including saves and config)
-Remove-Item $unzipDir\* -Force -Recurse
-
-# Move saves and config back to the game directory
-try {Move-Item -Path $tempZelda3 -Destination $unzipDir} catch {Write-Warning "Could not move save files and config from $env:TEMP\zelda3 back to $unzipDir\zelda3."}
-Remove-Item $tempZelda3 -Recurse -Force
-
-try {Remove-Item "C:\Games\Zelda 3 Launcher\triforce.ico"} catch {}
-try {Remove-Item "$env:UserProfile\Desktop\Zelda 3 Launcher.lnk"} catch {}
-try {Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Zelda 3 Launcher.lnk"} catch {}
-
-# Delete empty installation directories.
-
-# This directory is not likely to both exist and be empty, but just in case.
-try {
-  $FolderNotEmpty = Test-Path -Path $saveDir\*
-  if (-Not $FolderNotEmpty){
+# Remove Save directory if empty (not likely though)
+$Exists = Test-Path -Path $saveDir
+$Empty = -Not $(Test-Path -Path $saveDir/*)
+if ($Empty){
     Remove-Item $saveDir
-  }
-} catch {}
+}
 
-# This directory is not likely to both exist and be empty, but just in case.
-try {
-  $FolderNotEmpty = Test-Path -Path $zelda3Dir\*
-  if (-Not $FolderNotEmpty){
+$Exists = Test-Path -Path $zelda3Dir
+$Empty = -Not $(Test-Path -Path $zelda3Dir/*)
+
+if ($Exists -And -Not $Empty){
+    # Move saves and confg to temporary location (because Powershell is hell)
+    New-Item -ItemType Directory -Path $tempZelda3
+    Write-Warning "Created directory."
+    # Start-Sleep 5
+    
+    # Remove reference save files
+    $Exists = Test-Path -Path $saveDirRef
+    if ($Exists) {
+        Remove-Item $saveDirRef -Recurse -Force
+    }
+
+    # Move Saves to temporary location
+    $Exists = Test-Path -Path $saveDir
+    if ($Exists) {
+        Move-Item -Path $saveDir -Destination $tempZelda3
+    }
+
+    # Move config to temporary location
+    $Exists = Test-Path -Path $config
+    if ($Exists) {
+        Move-Item -Path $config -Destination $tempZelda3
+    }
+
+    # Remove everything in the launcher directory, including zelda3
+    Remove-Item $unzipDir/* -Force -Recurse
+
+    # Move saves and config back to the game directory
+    Move-Item -Path $tempZelda3 -Destination $unzipDir
+
+}
+
+$Exists = Test-Path -Path $zelda3Dir
+$Empty = -Not $(Test-Path -Path $zelda3Dir/*)
+
+if ($Exists -And -Not $Empty){
     Remove-Item $zelda3Dir
-  }
-} catch {}
+}
 
-try {
-  $FolderNotEmpty = Test-Path -Path $unzipDir\*
-  if (-Not $FolderNotEmpty){
-    Remove-Item $unzipDir
-  }
-} catch {}
+# Remove desktop shortcut if it exist (it's plausible the user might have removed it)
+$Exists = Test-Path -Path "$env:UserProfile/Desktop/Zelda 3 Launcher.lnk"
 
-# This directory is not likely exist at this point, but just in case.
-try {
-  $FolderNotEmpty = Test-Path -Path $tempZelda3\*
-  if (-Not $FolderNotEmpty){
-    Remove-Item $unzipDir
-  }
-} catch {}
+if ($Exists){
+    Remove-Item "$env:UserProfile/Desktop/Zelda 3 Launcher.lnk"
+}
+
+# Remove start menu shortcut if it exist (it's plausible the user might have removed it)
+$Exists = Test-Path -Path "$env:ProgramData/Microsoft/Windows/Start Menu/Programs/Zelda 3 Launcher.lnk"
+
+if ($Exists){
+    Remove-Item "$env:ProgramData/Microsoft/Windows/Start Menu/Programs/Zelda 3 Launcher.lnk"
+}
 
 
 Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'Zelda_3_Launcher_v1.3.6.0.zip'
