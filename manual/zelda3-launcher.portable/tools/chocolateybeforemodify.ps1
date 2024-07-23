19de530f412e14d25dc5220541c5108425dfb663
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-23 23:23:01 +0200

    Replaced try catch statements with `-ea 0`

--- manual/zelda3-launcher.portable/tools/chocolateybeforemodify.ps1
+++ manual/zelda3-launcher.portable/tools/chocolateybeforemodify.ps1
@@ -1,17 +1,1 @@
-﻿# Unable to get this to work.
-
-# get zelda3 process
-$process = Get-Process "zelda3" -ea 0
-
-# stop the process
-if ($process) {
-    $process | Stop-Process -Force
-}
-
-# get launcher process
-$process = Get-Process "Zelda 3 Launcher" -ea 0
-
-# stop the process
-if ($process) {
-    $process | Stop-Process -Force
-}
+﻿Start-CheckandStop "Zelda 3 Launcher"
