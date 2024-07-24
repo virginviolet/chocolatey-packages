a1489f67ea896c2b8291d72533420de8a0f39553
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-24 14:04:59 +0200

    Improvements to process stopping code

--- manual/png-to-ico.install/tools/chocolateyuninstall.ps1
+++ manual/png-to-ico.install/tools/chocolateyuninstall.ps1
@@ -1,23 +1,5 @@
 $ErrorActionPreference = 'Stop'
 
-# Specify the name or path of a .bat file to terminate
-$targetBatFile = "png_to_ico.bat"
-
-# Get all cmd processes
-$cmdProcesses = Get-WmiObject -Query "SELECT * FROM Win32_Process WHERE Name = 'cmd.exe'"
-
-foreach ($process in $cmdProcesses) {
-    # Get the command line arguments of the process
-    $commandLine = $process.CommandLine
-
-    # Check if the command line contains the target .bat file
-    if ($commandLine -like "*$targetBatFile*") {
-        Write-Host "Terminating process $($process.ProcessId) running $targetBatFile" 2> $null
-        # Terminate the process
-        Stop-Process -Id $process.ProcessId -Force 2> $null
-    }
-}
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'PNG-to-ICO'
