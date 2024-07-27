78c977901ad8ed1f9234525eb3b5c282f67e425a
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-07-27 23:28:53 +0200

    Template

---
+++ manual/flips/tools/chocolateyinstall.ps1
@@ -0,0 +1,58 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+# In Chocolatey scripts, ALWAYS use absolute paths - $toolsDir gets you to the package's tools directory.
+$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipArchive = Join-Path $toolsDir -ChildPath 'example.zip'
+
+$unzipArgs = @{
+  FileFullPath = $zipArchive
+  Destination = $toolsDir
+}
+
+## Helper functions - these have error handling tucked into them already
+## see https://docs.chocolatey.org/en-us/create/functions
+
+## Unzips a file to the specified location - auto overwrites existing content
+## - https://docs.chocolatey.org/en-us/create/functions/get-chocolateyunzip
+Get-ChocolateyUnzip @unzipArgs
+
+## To avoid quoting issues, you can also assemble your -Statements in another variable and pass it in
+#$appPath = "$env:ProgramFiles\appname"
+##Will resolve to C:\Program Files\appname
+#$statementsToRun = "/C `"$appPath\bin\installservice.bat`""
+#Start-ChocolateyProcessAsAdmin $statementsToRun cmd -validExitCodes $validExitCodes
+
+## add specific folders to the path - any executables found in the chocolatey package
+## folder will already be on the path.
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateypath
+#Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
+
+## Add specific files as shortcuts to the desktop
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyshortcut
+#$target = Join-Path $toolsDir "$($packageName).exe"
+# Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>" [-workDirectory "C:\" -arguments "C:\test.txt" -iconLocation "C:\test.ico" -description "This is the description"]
+
+## Outputs the bitness of the OS (either "32" or "64")
+## - https://docs.chocolatey.org/en-us/create/functions/get-osarchitecturewidth
+#$osBitness = Get-ProcessorBits
+
+## Set persistent Environment variables
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyenvironmentvariable
+#Install-ChocolateyEnvironmentVariable -variableName "SOMEVAR" -variableValue "value" [-variableType = 'Machine' #Defaults to 'User']
+
+## Set up a file association
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyfileassociation
+#Install-ChocolateyFileAssociation
+
+## Adding a shim when not automatically found - Chocolatey automatically shims exe files found in package directory.
+## - https://docs.chocolatey.org/en-us/create/functions/install-binfile
+## - https://docs.chocolatey.org/en-us/create/create-packages#how-do-i-exclude-executables-from-getting-shims
+#Install-BinFile
+
+## Other needs: use regular PowerShell to do so or see if there is a function already defined
+# - https://docs.chocolatey.org/en-us/create/functions
+# There may also be functions available in extension packages
+# - https://community.chocolatey.org/packages?q=id%3A.extension for examples and availability.
