a6fd13059e21dbe7cd46cbb9411d40ff9d689d7d
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-08-27 14:27:17 +0200

    Install script now adds friendly app name

--- manual/flips/tools/chocolateyinstall.ps1
+++ manual/flips/tools/chocolateyinstall.ps1
@@ -8,28 +8,33 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $zipArchive = Join-Path $toolsDir -ChildPath 'flips-windows.zip'
 $unzipDir = Join-Path $toolsDir -ChildPath 'flips-windows'
 $executableDir = Join-Path $unzipDir -ChildPath 'builds\windows-x64-gui.zip'
-$executable = Join-Path $executableDir "flips.exe"
+$exe = "flips.exe"
+$executable = Join-Path $executableDir -ChildPath $exe
 
 $unzipArgs = @{
   FileFullPath = $zipArchive
   Destination  = $unzipDir
 }
 
-## TODO: "flips.exe" → "Floating IPS"
-
-## Helper functions - these have error handling tucked into them already
-## see https://docs.chocolatey.org/en-us/create/functions
-
-## Unzips a file to the specified location - auto overwrites existing content
+## Unzips file to the specified location - auto overwrites existing content
 Get-ChocolateyUnzip @unzipArgs
+
+# Add friendly app name (default is 'flips.exe')
+# for current user only, because... I don't know, then I won't have to rewrite a function in removeregistrykeys.ps1 and make it more complicated.
+$friendlyAppName = "Floating IPS"
+$keyPath = "REGISTRY::HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache"
+$valueName = Join-Path $executableDir -ChildPath "$exe.FriendlyAppName"
+$valueData = $friendlyAppName
+$valueOk = [bool](Get-ItemProperty -Path $keyPath -Name $valueName -ea 0)
+if ($valueOk) {
+  Set-ItemProperty -Path $keyPath -Name $valueName -Value $valueData -Type STRING
+}
+else {
+  New-ItemProperty -Path $keyPath -Name $valueName -Value $valueData -Type STRING
+}
 
 ## Add desktop shortcut
 Install-ChocolateyShortcut -shortcutFilePath "$env:UserProfile\Desktop\Floating IPS.lnk" -targetPath $executable -workingDirectory $executableDir -description "Apply IPS and BPS patches."
 
 ## Add start menu shortcut
 Install-ChocolateyShortcut -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Floating IPS.lnk" -targetPath $executable -WorkingDirectory $executableDir -description "Apply IPS and BPS patches."
-
-## Other needs: use regular PowerShell to do so or see if there is a function already defined
-# - https://docs.chocolatey.org/en-us/create/functions
-# There may also be functions available in extension packages
-# - https://community.chocolatey.org/packages?q=id%3A.extension for examples and availability.
