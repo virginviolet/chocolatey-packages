c4ae7edd59c461efd6ec7353b8aa0f2ea6ad1d98
mimmla <179630708+mimmla@users.noreply.github.com>
2024-08-28 20:51:43 +0200

    Revert "Working on fix for backup inadvertently removed"
    This reverts commit 01c5718fae1a7d1e2aa20c4137e981d1e5e10e7e.

--- manual/zelda3-launcher/tools/chocolateyuninstall.ps1
+++ manual/zelda3-launcher/tools/chocolateyuninstall.ps1
@@ -7,7 +7,6 @@ $saveDir    = Join-Path $zelda3Dir 'saves'
 $saveDirRef = Join-Path $saveDir 'ref'
 $config     = Join-Path $zelda3Dir 'zelda3.ini'
 $tempZelda3 = Join-Path $env:TEMP 'zelda3.bak'
-$backupZelda3 = Join-Path $unzipDir 'zelda3.bak'
 
 
 # Prevent uninstall if the game is running, so that no progress is lost
@@ -20,66 +19,50 @@ Start-CheckandThrow "zelda3"
 # when Test-Path checks a path that doesn't exist.
 # We could skip checking if directory exists and just use `-ea 0` for the main
 # command, but then we won't catch any errors at all.
-$exists = [bool](Test-Path -Path $saveDirRef -ea 0)
-$empty = -Not ([bool](Test-Path -Path $saveDirRef\* -ea 0))
+$exists = Test-Path -Path $saveDirRef -ea 0
+$empty = -Not (Test-Path -Path $saveDirRef\* -ea 0)
 if ($exists -And $empty){
     Remove-Item $saveDirRef
 }
 
 # Remove save directory if empty
-$exists = [bool](Test-Path -Path $saveDir -ea 0)
-$empty = -Not ([bool](Test-Path -Path $saveDir\* -ea 0))
+$exists = Test-Path -Path $saveDir -ea 0
+$empty = -Not (Test-Path -Path $saveDir\* -ea 0)
 if ($exists -And $empty){
     Remove-Item $saveDir
 }
 
-# Remove zelda3 directory (game directory)
-$exists = [bool](Test-Path -Path $zelda3Dir -ea 0)
-$empty = -Not ([bool](Test-Path -Path $zelda3Dir\* -ea 0))
+$exists = Test-Path -Path $zelda3Dir -ea 0
+$empty = -Not (Test-Path -Path $zelda3Dir\* -ea 0)
 if ($exists -And -Not $empty){
     Write-Warning 'Zelda 3 will be removed.'
     Write-Warning 'Settings and save data will be backed up.'
     Pause # Pause to abort
+    # Move save data and confg to temporary location (because Powershell is hell)
+    New-Item -ItemType Directory -Path $tempZelda3
     
     # Remove reference save files
-    $exists = [bool](Test-Path -Path $saveDirRef -ea 0)
+    $exists = Test-Path -Path $saveDirRef -ea 0
     if ($exists) {
         Remove-Item $saveDirRef -Recurse -Force
     }
-    
-    $backupExists = [bool](Test-Path -Path $backupZelda3 -ea 0)
-    $backupEmpty = -Not ([bool](Test-Path -Path $backupZelda3\* -ea 0))
-    $savesExists = [bool](Test-Path -Path $tempZelda3 -ea 0)
-    $configExists = [bool](Test-Path -Path $config -ea 0)
-    
-    # Move either existing backup, or save data and confg, to temporary location (because Powershell is hell)
-    if ($backupExists -And (-Not $backupEmpty)) {
-        if ($savesExists -Or $configExists) {
-            Write-Warning "Existing backup found. Overwrite?"
-            New-Item -ItemType Directory -Path $tempZelda3 # TODO replace with `Move-Item -Path`?
-            # XXX
-            # Pause 30 seconds, throw
-            # if no: Move existing backup to $tempZelda3 (because Powershell is hell)
-        }
+
+    # Move save data to temporary location
+    $exists = Test-Path -Path $saveDir -ea 0
+    if ($exists) {
+        Move-Item -Path $saveDir -Destination $tempZelda3
     }
-    else {
-        if ($savesExists) {
-            # Move save data to temporary location (because Powershell is hell)
-            New-Item -ItemType Directory -Path $tempZelda3 # TODO replace with `Move-Item -Path`?
-            Move-Item -Path $saveDir -Destination $tempZelda3
-        }
-        if ($configExists) {
-            # Move config to temporary location (because Powershell is hell)
-            New-Item -ItemType Directory -Path $tempZelda3 # TODO replace with `Move-Item -Path`?
-            Move-Item -Path $config -Destination $tempZelda3
-        }
+
+    # Move config to temporary location
+    $exists = Test-Path -Path $config -ea 0
+    if ($exists) {
+        Move-Item -Path $config -Destination $tempZelda3
     }
 
     # Remove everything in the launcher directory, including zelda3
     Remove-Item $unzipDir\* -Force -Recurse
 
     # Move save data and config back to the game directory
-    # If the zelda3 folder exists, the launcher will think the game is built, so we let the folder be named $tempZelda3 istead. 
     Move-Item -Path $tempZelda3 -Destination $unzipDir
 
     Write-Warning 'Manually remove the configuration and save data backup if so desired. (See this package''s description for more details.)'
@@ -90,13 +73,13 @@ if ($exists -And -Not $empty){
     }
 
 # Remove desktop shortcut if it exists (it's plausible the user might have removed it)
-$exists = [bool](Test-Path -Path "$env:UserProfile\Desktop\Zelda 3 Launcher.lnk" -ea 0)
+$exists = Test-Path -Path "$env:UserProfile\Desktop\Zelda 3 Launcher.lnk" -ea 0
 if ($exists) {
     Remove-Item "$env:UserProfile\Desktop\Zelda 3 Launcher.lnk"
 }
 
 # Remove start menu shortcut if it exists (it's plausible the user might have removed it)
-$exists = [bool](Test-Path -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Zelda 3 Launcher.lnk" -ea 0)
+$exists = Test-Path -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Zelda 3 Launcher.lnk" -ea 0
 if ($exists) {
     Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Zelda 3 Launcher.lnk"
 }
