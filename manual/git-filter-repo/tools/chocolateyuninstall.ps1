9feb71da61e4fb6ca7e8303ccb30f8fa6f212678
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-09-28 17:21:34 +0200

    Fresh template

---
+++ manual/git-filter-repo/tools/chocolateyuninstall.ps1
@@ -0,0 +1,23 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
+## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
+## See https://docs.chocolatey.org/en-us/choco/commands/uninstall
+## and https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateypackage
+## and https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
+
+$ErrorActionPreference = 'Stop' # stop on all errors
+Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'example.zip' # Only necessary if you did not unpack to package directory - see https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
+
+
+# Uninstall-ChocolateyZipPackage will remove the FILES from the archive.
+# This removes the DIRECTORY they were extracted too.
+Remove-Item 'C:\example'
+
+## OTHER POWERSHELL FUNCTIONS
+## https://docs.chocolatey.org/en-us/create/functions
+#Uninstall-ChocolateyEnvironmentVariable - https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyenvironmentvariable
+#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://docs.chocolatey.org/en-us/create/functions/uninstall-binfile
+## Remove any shortcuts you added in the install script.
