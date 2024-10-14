3931387f0531143fd77aa53bf14e14592a5e54f8
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-15 00:52:31 +0200

    Cleaned out comments; filled out VERIFICATION.txt

--- manual/zelda-classic/tools/chocolateyinstall.ps1
+++ manual/zelda-classic/tools/chocolateyinstall.ps1
@@ -1,6 +1,4 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿# Install Zelda Classic
 
 # Preferences
 $ErrorActionPreference = 'Stop' # Stop on all errors
@@ -14,34 +12,31 @@ $shortcutBaseNameZQuest = "ZQuest"
 $StartMenuDirectory = "Zelda Classic\"
 $desktopDirectory = ""
 
-# Prevent uninstall if the game is running, so that no progress is lost
+# Prevent force install or upgrade if the game is running (so that no progress is lost)
 # This cannot be moved to chocolateybeforemodify.ps1 unless the feature suggested in the following issue is added:
 # https://github.com/chocolatey/choco/issues/1731
-# [x] Test
 Start-CheckandThrow "zelda" > $null
 
-# Prevent uninstall if the quest editor is running, so that no progress is lost
-# [x] Test
+# Prevent force install or upgrade if the quest editor is running (so that no progress is lost)
 Start-CheckandStop "zquest" > $null
 
 # Stop the launcher
-# [x] Test
 Start-CheckandStop "zlaunch" > $null
 
 # Stop the launcher
-# [x] Test
 Start-CheckandStop "romview-w" > $null
 
 # Extract archive
-# [x] Test
+# Paths
 $toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $zipArchivePath = Join-Path $toolsDirPath -ChildPath '2.53_Win_Release_2-17APRIL2019.zip'
+# Arguments
 $unzipArgs = @{
   PackageName  = "zelda-classic"
   FileFullPath = $zipArchivePath
   Destination  = $installationDirPath
 }
-# Extract archive
+# Extract
 Get-ChocolateyUnzip @unzipArgs
 
 ## Add shortcuts
@@ -102,17 +97,3 @@ if ($installStartMenuShortcuts) {
     "$shortcutStart" | Out-File "$shortcutsLog" -Append
   }
 }
-
-## Helper functions - these have error handling tucked into them already
-## see https://docs.chocolatey.org/en-us/create/functions
-
-## To avoid quoting issues, you can also assemble your -Statements in another variable and pass it in
-#$appPath = "$env:ProgramFiles\appname"
-##Will resolve to C:\Program Files\appname
-#$statementsToRun = "/C `"$appPath\bin\installservice.bat`""
-#Start-ChocolateyProcessAsAdmin $statementsToRun cmd -validExitCodes $validExitCodes
-
-## add specific folders to the path - any executables found in the chocolatey package
-## folder will already be on the path.
-## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateypath
-#Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
