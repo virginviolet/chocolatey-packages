dfe319c45b30eabcda2e828ab948d65d441ddfab
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-16 03:41:14 +0200

    templates: Tweaked scripts

--- templates/embedded-portable-zip.template/tools/chocolateyuninstall.ps1
+++ templates/embedded-portable-zip.template/tools/chocolateyuninstall.ps1
@@ -1,6 +1,4 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿# Additional uninstallation steps for Chocolatey
 
 ## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
 ## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
@@ -8,13 +6,15 @@
 ## and https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateypackage
 ## and https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
 
-$ErrorActionPreference = 'Stop' # stop on all errors
-Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'example.zip' # Only necessary if you did not unpack to package directory - see https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
+# Preferences
+$ErrorActionPreference = 'Stop' # Stop on all errors
 
+# Remove extracted files
+Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName 'example.zip' # Only necessary if you did not unpack to package directory - see https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
 
 # Uninstall-ChocolateyZipPackage will remove the FILES from the archive.
 # This removes the DIRECTORY they were extracted too.
-Remove-Item 'C:\example'
+Remove-Item 'C:\example\'
 
 ## OTHER POWERSHELL FUNCTIONS
 ## https://docs.chocolatey.org/en-us/create/functions
