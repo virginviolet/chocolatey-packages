8b2987398c3a48bdab55697b50b07a1660539a1e
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-17 23:38:27 +0200

    embedded-install-exe: adapted uninstall script

--- templates/embedded-install-exe.template/tools/chocolateyuninstall.ps1
+++ templates/embedded-install-exe.template/tools/chocolateyuninstall.ps1
@@ -1,24 +1,15 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
+﻿## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
 ## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
 ## See https://docs.chocolatey.org/en-us/choco/commands/uninstall
 ## and https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateypackage
 
-## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
 ## If this is an exe, change fileType, silentArgs, and validExitCodes
 
 $ErrorActionPreference = 'Stop' # stop on all errors
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'embedded-install-exe.template*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: MSI or EXE (ignore MSU for now)
-  # MSI
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
-  # OTHERS
+  fileType      = 'EXE' # Only one of these: MSI or EXE (ignore MSU for now)
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
   #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
@@ -38,26 +29,12 @@ $packageArgs = @{
 
 if ($key.Count -eq 1) {
   $key | % {
-    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
-
-    if ($packageArgs['fileType'] -eq 'MSI') {
-      # The Product Code GUID is all that should be passed for MSI, and very
-      # FIRST, because it comes directly after /x, which is already set in the
-      # Uninstall-ChocolateyPackage msiargs (facepalm).
-      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-
-      # Don't pass anything for file, it is ignored for msi (facepalm number 2)
-      # Alternatively if you need to pass a path to an msi, determine that and
-      # use it instead of the above in silentArgs, still very first
-      $packageArgs['file'] = ''
-    } else {
-      # NOTES:
-      # - You probably will need to sanitize $packageArgs['file'] as it comes from the registry and could be in a variety of fun but unusable formats
-      # - Split args from exe in $packageArgs['file'] and pass those args through $packageArgs['silentArgs'] or ignore them
-      # - Ensure you don't pass double quotes in $file (aka $packageArgs['file']) - otherwise you will get "Illegal characters in path when you attempt to run this"
-      # - Review the code for auto-uninstaller for all of the fun things it does in sanitizing - https://github.com/chocolatey/choco/blob/bfe351b7d10c798014efe4bfbb100b171db25099/src/chocolatey/infrastructure.app/services/AutomaticUninstallerService.cs#L142-L192
-    }
-
+    # - You probably will need to sanitize $packageArgs['file'] as it comes from the registry and could be in a variety of fun but unusable formats
+    # - Ensure you don't pass double quotes in $file (aka $packageArgs['file']) - otherwise you will get "Illegal characters in path when you attempt to run this"
+    $packageArgs['file'] = "$($_.UninstallString)" # NOTE: You may need to split this if it contains spaces
+    # - Split args from exe in $packageArgs['file'] and pass those args through $packageArgs['silentArgs'] or ignore them
+    # - Review the code for auto-uninstaller for all of the fun things it does in sanitizing - https://github.com/chocolatey/choco/blob/bfe351b7d10c798014efe4bfbb100b171db25099/src/chocolatey/infrastructure.app/services/AutomaticUninstallerService.cs#L142-L192
+    $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
     Uninstall-ChocolateyPackage @packageArgs
   }
 } elseif ($key.Count -eq 0) {
@@ -69,10 +46,37 @@ if ($key.Count -eq 1) {
   $key | % {Write-Warning "- $($_.DisplayName)"}
 }
 
-## OTHER POWERSHELL FUNCTIONS
-## https://docs.chocolatey.org/en-us/create/functions
-#Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
+# Remove persistent Environment variable
 #Uninstall-ChocolateyEnvironmentVariable - https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyenvironmentvariable
-#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://docs.chocolatey.org/en-us/create/functions/uninstall-binfile
-## Remove any shortcuts you added in the install script.
 
+# Remove shim
+#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://docs.chocolatey.org/en-us/create/functions/uninstall-binfile
+
+##Remove shortcuts
+##Look for shortcuts log
+# $packagePath = $env:ChocolateyPackageFolder
+# $shortcutsLogPath = Join-Path "$packagePath" -ChildPath "shortcuts.txt"
+# $exists = Test-Path -Path "$shortcutsLogPath" -PathType Leaf
+# if ($removeShortcuts -and -not $exists) {
+#     Write-Warning "Cannot uninstall shortcuts.`nShortcuts log not found."
+# }
+# elseif ($exists) {
+#     Write-Debug "Shortcuts log found."
+#     # Read log line-per-line and remove files
+#     $shortcutsLog = Get-Content "$shortcutsLogPath"
+#     foreach ($fileInLog in $shortcutsLog) {
+#         if ($null -ne $fileInLog -and '' -ne $fileInLog.Trim()) {
+#             try {
+#                 Write-Debug "Removing shortcut '$fileInLog'."
+#                 Remove-Item -Path "$fileInLog" -Force
+#                 Write-Debug "Removed shortcut '$fileInLog'."
+#             }
+#             catch {
+#                 Write-Warning "Could not remove shortcut '$fileInLog'.`n$_"
+#             }
+#         }
+#     }
+# }
+
+## OTHER HELPER FUNCTIONS
+## https://docs.chocolatey.org/en-us/create/functions
