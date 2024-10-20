f3f1dc3c49231e0b7ab07aafe1b5c10c268a5457
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-20 16:47:42 +0200

    gnubg: Fresh template

---
+++ manual/gnubg/_TODO.md
@@ -0,0 +1,52 @@
+﻿# TODO
+
+## Guides
+
+- "Create Packages". <https://docs.chocolatey.org/en-us/create/create-packages>
+
+## To do
+
+1. [ ] Search the Community Repository to make sure there isn't a package for this software already: <https://community.chocolatey.org/packages>
+2. [ ] Make sure the total nupkg package will be under 200MB.
+3. [ ] Commit.
+4. [ ] Add installer EXE to the tools directory.
+5. [ ] Fill out legal\LICENSE.txt
+6. [ ] Fill out legal\VERIFICATION.txt
+7. [ ] Fill out tools\chocolateyBeforeModify.ps1 - remove if you have no processes or services to shut down before upgrade/uninstall
+8. [ ] Fill out tools\chocolateyInstall.ps1.
+9.  [ ] Fill out tools\chocolateyUninstall.ps1 - remove if autouninstaller can automatically uninstall and you have nothing additional to do during uninstall.
+10. [ ] Remove tools\.skipAutoUninstall if chocolateyUninstall.ps1 is used and auto-uninstaller is not desired.
+11. [ ] Fill out nuspec.
+12. [ ] Test the package to ensure install/uninstall work appropriately.
+13. [ ] Add nupkg and installer and to .gitignore.
+14. [ ] Commit.
+15. [ ] Clean out the comments you are not using in tools\chocolateybeforeModify.ps1.
+16. [ ] Clean out the comments and sections you are not using in tools\chocolateyInstall.ps1.
+17. [ ] Clean out the comments and sections you are not using in tools\chocolateyUninstall.ps1.
+18. [ ] Clean out all the comments in nuspec (you may wish to leave the headers for the package vs software metadata).
+19. [ ] Inform of default install location in description in nuspec.
+20. [ ] Finish legal\VERIFICATION.txt
+21. [ ] Delete ReadMe.md file once you have read over and used anything you've needed from here.
+22. [ ] Test the package again to ensure install/uninstall work appropriately.
+23. [ ] Commit and publish to GitHub.
+24. [ ] Merge branch with main.
+25. [ ] Add icon key to nuspec.
+26. [ ] Test the package yet again to ensure install/uninstall work appropriately.
+27. [ ] Delete this file.
+28. [ ] Merge branch with main again.
+29. [ ] Commit and publish to GitHub again.
+30. [ ] Pack and push to Chocolatey Community Repository.
+
+## Learn more
+
+To learn more about Chocolatey packaging, go through the workshop at <https://github.com/chocolatey/chocolatey-workshop>
+You will learn about
+
+- General packaging
+- Customizing package behavior at runtime (package parameters)
+- Extension packages
+- Custom packaging templates
+- Setting up an internal Chocolatey.Server repository
+- Adding and using internal repositories
+- Reporting
+- Advanced packaging techniques when installers are not friendly to automation
---
+++ manual/gnubg/tools/chocolateyinstall.ps1
@@ -0,0 +1,155 @@
+﻿# Other steps for installing gnubg with Chocolatey
+
+# Preferences
+$ErrorActionPreference = 'Stop' # stop on all errors
+# $shortcutName = "$($packageName)"
+# $addDesktopShortcut = $true
+# $addStartMenuShortcut = $true
+# $logShortcuts = $true
+
+## Helper functions - these have error handling tucked into them already
+## see https://docs.chocolatey.org/en-us/create/functions
+
+## Outputs the bitness of the OS (either "32" or "64")
+## - https://docs.chocolatey.org/en-us/create/functions/get-osarchitecturewidth
+# $osBitness = Get-ProcessorBits
+
+## Install Visual Studio Package - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyvsixpackage
+# Install-ChocolateyVsixPackage $packageName $url [$vsVersion] [-checksum $checksum -checksumType $checksumType]
+# Install-ChocolateyVsixPackage @packageArgs
+
+## Extract archive
+## - https://docs.chocolatey.org/en-us/create/functions/get-chocolateyunzip
+## Paths
+## In Chocolatey scripts, ALWAYS use absolute paths
+# $toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+# $zipArchivePath = Join-Path $toolsDirPath -ChildPath 'example.zip'
+# # Arguments
+# $unzipArgs = @{
+#   PackageName  = "$($packageName)"
+#   FileFullPath = "$zipArchivePath"
+#   Destination  = "$toolsDirPath"
+# }
+## Unzip file to the specified location - auto overwrites existing content
+# Get-ChocolateyUnzip @unzipArgs
+
+# Run EXE installer
+# - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
+# In Chocolatey scripts, ALWAYS use absolute paths
+$toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$ExeInstallerPath = Join-Path $toolsDirPath 'NAME_OF_EMBEDDED_INSTALLER_FILE.EXE'
+# Arguments
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDirPath
+  fileType       = 'EXE'
+  file           = $ExeInstallerPath
+  softwareName   = 'gnubg*' # Display name as it appears in "Installed apps" or "Programs and Features".
+  # Checksums
+  checksum       = 'INSERT_CHECKSUM'
+  checksumType   = 'sha256' # Default is md5, can also be sha1, sha256 or sha512
+  checksum64     = 'INSERT_CHECKSUM'
+  checksumType64 = 'sha256' # Default is checksumType
+  # Silent arguments
+  # Uncomment matching installer type (sorted by most to least common)
+  # silentArgs   = '/S'           # NSIS
+  # silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  # silentArgs   = '/s'           # InstallShield
+  # silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
+  # silentArgs   = '/s'           # Wise InstallMaster
+  # silentArgs   = '-s'           # Squirrel
+  # silentArgs   = '-q'           # Install4j
+  # silentArgs   = '-s'           # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  # silentArgs   = ''             # None; make silent with input macro script like AutoHotKey (AHK)
+  #       https://community.chocolatey.org/packages/autohotkey.portable
+  # Exit codes indicating success
+  # validExitCodes = @(0) # NSIS
+  # validExitCodes = @(0) # Inno Setup
+  validExitCodes = @(0) # Insert other valid exit codes here
+}
+# Installer, will assert administrative rights
+Install-ChocolateyInstallPackage @packageArgs
+
+## Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
+## - https://docs.chocolatey.org/en-us/create/functions/start-chocolateyprocessasadmin
+# Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
+## To avoid quoting issues, you can also assemble your -Statements in another variable and pass it in
+# $appPath = "$env:ProgramFiles\appname"
+# #Will resolve to C:\Program Files\appname
+# $statementsToRun = "/C `"$appPath\bin\installservice.bat`""
+# Start-ChocolateyProcessAsAdmin $statementsToRun cmd -validExitCodes $validExitCodes
+
+## Add specific folders to the path
+## Any executables found in the chocolatey package folder will
+## already be on the path. This is used in addition to that or
+## for cases when a native installer doesn't add things to the path.
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateypath
+# Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
+
+## Set persistent Environment variables
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyenvironmentvariable
+# Install-ChocolateyEnvironmentVariable -variableName "SOMEVAR" -variableValue "value" [-variableType = 'Machine' #Defaults to 'User']
+
+## Adding a shim when not automatically found - Chocolatey automatically shims exe files found in package directory.
+## - https://docs.chocolatey.org/en-us/create/functions/install-binfile
+## - https://docs.chocolatey.org/en-us/create/create-packages#how-do-i-exclude-executables-from-getting-shims
+# Install-BinFile
+
+## Set up file association
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyfileassociation
+# Install-ChocolateyFileAssociation
+
+## Other needs: use regular PowerShell to do so, or see if it can be accomplished with the helper functions
+## - https://docs.chocolatey.org/en-us/create/functions
+## There may also be functions available in extension packages
+## - https://community.chocolatey.org/packages?q=id%3A.extension for examples and availability.
+
+## Add shortcuts
+## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyshortcut
+# if ($addDesktopShortcut -or $addStartMenuShortcut) {
+#   # Paths
+#   $executableDirPath = $toolsDirPath
+#   $executablePath = Join-Path $executableDirPath "$($packageName).exe"
+#   $desktopShortcutPath = "$env:UserProfile\Desktop\$shortcutName.lnk"
+#   $startMenuShortcutPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk"
+#   $iconPath = Join-Path "$installerDirPath" -ChildPath "$($packageName).ico"
+#   if ($logShortcuts) {
+#     $packagePath = $env:ChocolateyPackageFolder
+#     $shortcutsLog = Join-Path "$packagePath" -ChildPath "shortcuts.txt"
+#   }
+# }
+## Add desktop shortcut
+# if ($addDesktopShortcut) {
+#   # Arguments
+#   $desktopShortcutArgs = @{
+#     shortcutFilePath = "$desktopShortcutPath"
+#     targetPath       = "$executablePath"
+#     workingDirectory = "$installerDirPath"
+#     arguments        = "C:\test.txt"
+#     iconLocation     = "$iconPath"
+#     description      = "This is the description."
+#   }
+#   Install-ChocolateyShortcut @desktopShortcutArgs
+#   # Log
+#   if ($logShortcuts) {
+#     "$desktopShortcutPath" | Out-File "$shortcutsLog" -Append
+#   }
+# }
+## Add start menu shortcut
+# if ($addStartMenuShortcut) {
+#   # Arguments
+#   $startMenuShortcutArgs = @{
+#     shortcutFilePath = "$startMenuShortcutPath"
+#     targetPath       = "$executablePath"
+#     workingDirectory = "$installerDirPath"
+#     arguments        = "C:\test.txt"
+#     iconLocation     = "$iconPath"
+#     description      = "This is the description."
+#   }
+#   Install-ChocolateyShortcut @startMenuShortcutArgs
+#   # Log
+#   if ($logShortcuts) {
+#     "$startMenuShortcutPath" | Out-File "$shortcutsLog" -Append
+#   }
+# }
