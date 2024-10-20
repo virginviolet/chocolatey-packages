00d5c15249f02a9a8d01e0c93fcd083bea08046e
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-20 17:40:34 +0200

    pysolfc: Updated hashes; Added _TODO_upgrade.md

---
+++ manual/pysolfc/_TODO_upgrade.md
@@ -0,0 +1,15 @@
+- [ ] `Tools/`: Move binary to `archive/`.
+- [ ] `pysolfc/`: Move nupkg to `archive/`.
+- [ ] `Tools/`: Move new binary to `tools/`.
+- [ ] `Nuspec`: Update `version`.
+- [ ] `Nuspec`: Update `releaseNotes`.
+- [ ] `legal/LICENSE.txt`: Verify license.
+- [ ] `legal/VERIFICATION.txt`: Update download URL.
+- [ ] `legal/VERIFICATION.txt`: Update filenames in the two places.
+- [ ] `legal/VERIFICATION.txt`: Verify license URL.
+- [ ] `chocolateyInstall.ps1`: Change `$exeInstallerPath`.
+- [ ] `chocolateyInstall.ps1`: Change `checksum`.
+- [ ] Test the package to ensure install works appropriately.
+    `choco pack; choco upgrade pysofc --source .`
+- [ ] Push package.
+    `choco push`
--- manual/pysolfc/tools/chocolateyinstall.ps1
+++ manual/pysolfc/tools/chocolateyinstall.ps1
@@ -5,23 +5,22 @@ $ErrorActionPreference = 'Stop' # stop on all errors
 
 # Run EXE installer
 $toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$ExeInstallerPath = Join-Path $toolsDirPath 'PySolFC_3.1.0_setup.exe'
+$exeInstallerPath = Join-Path $toolsDirPath 'PySolFC_3.1.0_setup.exe'
 # Arguments
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDirPath
   fileType       = 'EXE'
-  file           = $ExeInstallerPath
-  softwareName   = 'PySol Fan Club edition*' # Display Name as it appears in "Installed apps" or "Programs and Features".
+  file           = $exeInstallerPath
+  softwareName   = 'PySol Fan Club edition*' # Display name as it appears in "Installed apps" or "Programs and Features".
   # Checksums
-  checksum       = '66DEB447B2FDC9EA1B32E4E1A920A4BE69301C11CB8337E10CCFBA76AB5CB42F'
-  checksumType   = 'sha256' # Default is md5, can also be sha1, sha256 or sha512
+  checksum       = 'CDAE0B60A48C6A7C763BC549AA71FB52C586E2C5C4652B78DD4178953020F489'
+  checksumType   = 'sha256'
   # No 64-bit version has yet been distributed
   # checksum64     = ''
-  # checksumType64 = 'sha256' # Default is checksumType
+  # checksumType64 = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes = @(0) # Inno Setup
 }
 ## Installer, will assert administrative rights
-## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
-Install-ChocolateyInstallPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
+Install-ChocolateyInstallPackage @packageArgs
