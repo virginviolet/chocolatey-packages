f189460b76a7dd7b0c875129e4ace145440e53b0
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-23 02:50:52 +0200

    gnubg: Filled out chocolateyuninstall

--- manual/gnubg/tools/chocolateyuninstall.ps1
+++ manual/gnubg/tools/chocolateyuninstall.ps1
@@ -8,15 +8,76 @@
 # Preferences
 $ErrorActionPreference = 'Stop' # Stop on all errors
 
+# Prevent uninstall if any of these programs are running
+# (so that no progress is lost)
+# This cannot be moved to chocolateybeforemodify.ps1
+# unless the feature suggested in the following issue is added:
+# https://github.com/chocolatey/choco/issues/1731
+# GUI version of the game
+Start-CheckandThrow "gnubg"
+# Command line version of the game
+Start-CheckandThrow "gnubg-cli"
+# Bundled program for generating bearoff databases
+Start-CheckandThrow "makebearoff"
+# Bundled program for generating databases for Hypergammon
+Start-CheckandThrow "makehyper"
+# Bundled program for generating a GNU Backgammon binary weights file
+Start-CheckandThrow "makeweights"
+
+# Close the following program if it is running
+# Bundled program for dumping a position from the
+# GNU Backgammon bearoff database
+# Check PowerShell version
+# (Start-CheckandStop uses Write-Host, so output can only be suppressed
+# in PowerShell > 5.0)
+if ($PSVersionTable.PSVersion.Major -ge 5) {
+  # Redirect to Success stream before $null,
+  # in case the author ever decides to move away from Write-Host
+  Write-Debug "PowerShell >= 5.0"
+  Start-CheckandStop "bearoffdump" 6>&1 > $null
+} else {
+  Write-Debug "PowerShell < 5.0"
+  Start-CheckandStop "bearoffdump" > $null
+}
+
+# Remove empty GNUbg preferences folders
+# (This mitigates the installer showing a GUI prompt when GNUbg preferences directory exists)
+# Remove '.gnubg/backup/' directory if it exists and is empty
+$gnuBgPreferencesDir = Join-Path "$env:UserProfile" -ChildPath '.gnubg/'
+$gnuBgPreferencesBackupDir = Join-Path "$gnuBgPreferencesDir" -ChildPath 'backup/'
+$exists = Test-Path "$gnuBgPreferencesBackupDir" -PathType Container
+$empty = -not ([bool](Test-Path "$gnuBgPreferencesBackupDir\*"))
+# Pause
+if ($exists -and $empty) {
+  # [x] Test
+  Write-Debug "Empty GNU Backgammon preferences backup directory found. Removing..."
+  # Pause
+  Remove-Item $gnuBgPreferencesBackupDir
+  Write-Debug "Empty GNU Backgammon preferences backup directory removed."
+  # Pause
+}
+# Remove '.gnubg/' directory if it is empty
+$exists = Test-Path "$gnuBgPreferencesDir" -PathType Container
+$empty = -not ([bool](Test-Path "$gnuBgPreferencesDir\*"))
+# TODO Check if .gnubg/backup empty
+if ($exists -and $empty) {
+  # [x] Test
+  Write-Debug "Empty GNU Backgammon preferences directory found. Removing..."
+  # Pause
+  Remove-Item $gnuBgPreferencesDir
+  Write-Debug "Empty GNU Backgammon preferences directory removed."
+  # Pause
+}
+
 # Uninstall
-# Arguments for Get-UninstallRegistryKey and Uninstall-ChocolateyPackage
+# Arguments for 'Get-UninstallRegistryKey' and 'Uninstall-ChocolateyPackage'
 $packageArgs = @{
-  packageName  = $env:ChocolateyPackageName
-  softwareName = 'embedded-install-exe.template*' # Display name as it appears in "Installed apps" or "Programs and Features".
-  fileType     = 'EXE'
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'GNU Backgammon*' # Display name as it appears in "Installed apps" or "Programs and Features".
+  fileType       = 'EXE'
   # Uncomment matching installer type (sorted by most to least common)
   # silentArgs   = '/S'           # NSIS
-  # silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   # silentArgs   = '/s'           # InstallShield
   # silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
   # silentArgs   = '/s'           # Wise InstallMaster
@@ -28,7 +89,7 @@ $packageArgs = @{
   #       https://community.chocolatey.org/packages/autohotkey.portable
   # Exit codes indicating success
   # validExitCodes = @(0) # NSIS
-  # validExitCodes = @(0) # Inno Setup
+  validExitCodes = @(0) # Inno Setup
   # validExitCodes= @(0) # Insert other valid exit codes here
 }
 # Get uninstall registry keys that match the softwareName pattern
@@ -43,13 +104,82 @@ if ($keys.Count -eq 1) {
     # - Split args from exe in $packageArgs['file'] and pass those args through $packageArgs['silentArgs'] or ignore them
     # - Review the code for auto-uninstaller for all of the fun things it does in sanitizing - https://github.com/chocolatey/choco/blob/bfe351b7d10c798014efe4bfbb100b171db25099/src/chocolatey/infrastructure.app/services/AutomaticUninstallerService.cs#L142-L192
     $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+    # Move GNU Backgammon preferences directory to TEMP before uninstalling
+    # (otherwise, the installer will show a GUI prompt)
+    $shouldRestorePrefsDir = $false
+    $gnuBgPreferencesTempDir = Join-Path $env:TEMP -ChildPath '.gnubg/'
+    # Move directory if it exists
+    # XXX
+    # [x] Test
+    $exists = Test-Path "$gnuBgPreferencesDir" -PathType Container
+    if ($exists) {
+      # [x] Test
+      Write-Warning "GNU Backgammon preferences directory found."
+      Write-Warning "GNU Backgammon preferences directory will *not* be removed."
+      # Pause so that the user has time to read.
+      # Chocolatey stops Pause after 30 seconds by default.
+      Pause
+      # Edge case where there already exists a directory named '.gnubg/' in TEMP
+      # (likely from a failed previous uninstallation)
+      $gnuBgPreferencesTempDirExists = Test-Path "$gnuBgPreferencesTempDir" -PathType Container
+      if ($gnuBgPreferencesTempDirExists) {
+        # [x] Test
+        $message = "A GNU Backgammon preferences directory already exists in " `
+          + "the temporary direcotry. Please move or remove it, then run the " `
+          + "uninstallation again. `n" `
+          + "Location: '$gnuBgPreferencesTempDir'"
+        Write-Error "$message"
+      }
+      Write-Verbose "Moving '.gnubg/' from '$env:UserProfile' to '$env:TEMP'..."
+      Move-Item $gnuBgPreferencesDir -Destination "$env:TEMP" -Force
+      Write-Verbose "Moved '.gnubg/' from '$env:UserProfile' to '$env:TEMP'."
+      $shouldRestorePrefsDir = $true
+    } else {
+      # [x] Test
+      Write-Debug "GNU Backgammon preferences directory not found."
+      # Pause
+    }
     # Run uninstaller
-    Uninstall-ChocolateyPackage @packageArgs
+    if (-not $shouldRestorePrefsDir) {
+      # [x] Test
+      Write-Debug "Running installer..."
+      # Pause
+      Uninstall-ChocolateyPackage @packageArgs
+    } else {
+      # [x] Test
+      try {
+        Write-Debug "Running installer..."
+        # Pause
+        # throw
+        Uninstall-ChocolateyPackage @packageArgs
+      } catch {
+        # [x] Test
+        # Restore GNUbg preferences directory if installation failed.
+        Write-Verbose "Installation failed."
+        Write-Error "Could not install $env:ChocolateyPackageName.`n$_"
+        # Pause
+      } finally {
+        Write-Verbose "Restoring preferences directory..."
+        Write-Verbose "Moving '.gnubg/' from '$env:TEMP' to '$env:UserProfile'..."
+        # Pause
+        Move-Item $gnuBgPreferencesTempDir -Destination "$env:UserProfile" -Force # Replace silently in edge case where it already exists
+        Write-Verbose "Moved '.gnubg/' from '$env:TEMP' to '$env:UserProfile'."
+        Write-Verbose "Preferences directory restored."
+      }
+      # [x] Test
+      # Inform user if preferences exists.
+      $exists = Test-Path "$gnuBgPreferencesDir" -PathType Container
+      $message = "GNU Backgammon preferences directory remains.`n" `
+        + "Manually remove it if you do not wish to keep it.`n" `
+        + "GNU Backgammon preferences directory: $gnuBgPreferencesDir"
+      Write-Warning $message
+      Start-Sleep -Seconds 5 # time to read
+    }
   }
-# If 0 matches was found
+  # If 0 matches was found
 } elseif ($keys.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
-# If more than 1 matches were found
+  # If more than 1 matches were found
 } elseif ($keys.Count -gt 1) {
   Write-Warning "$($keys.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
