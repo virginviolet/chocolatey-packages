bfd5a4215ddc3311c1c90e1e62bc1bb5dec27985
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-10-28 01:02:19 +0100

    spacecadetpinball: Working on install script

--- manual/spacecadetpinball/tools/chocolateyinstall.ps1
+++ manual/spacecadetpinball/tools/chocolateyinstall.ps1
@@ -1,12 +1,16 @@
 ﻿# Other steps for installing spacecadetpinball with Chocolatey
 
-# Preferences
+# Initialization
 $ErrorActionPreference = 'Stop' # Stop on all errors
+$toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+# Preferences
+$installationDirPath = Join-Path "$toolsDirPath" '3D Pinball'
+# $installationDirPath = 'C:\Games\spacecadetpinball'
 $shortcutName = "$($packageName)"
 $addDesktopShortcut = $true
 $addStartMenuShortcut = $true
 $logShortcuts = $true
-# $installationDirPath = 'C:\Tools\spacecadetpinball'
 
 ## Helper functions
 ## These have error handling tucked into them already
@@ -24,19 +28,59 @@ $logShortcuts = $true
 # Install-ChocolateyVsixPackage $packageName $url [$vsVersion] [-checksum $checksum -checksumType $checksumType]
 # Install-ChocolateyVsixPackage @packageArgs
 
+# Download and unpack a zip file
+# Documentation - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyzippackage
+# Source code - 
+# Path
+$url = 'https://ia902303.us.archive.org/28/items/3d-pinball-x64/3D%20Pinball%20x64.zip'
+# Arguments
+$originalGameArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $installationDirPath
+  url           = $url
+  checksum      = 'AF6B1B5A3C0B9D13C58DFEFC937982A0357D209962A81003B11C8FA58EF56329'
+  checksumType  = 'sha256' # Default is md5, can also be sha1, sha256 or sha512
+  # checksum64    = 'AF6B1B5A3C0B9D13C58DFEFC937982A0357D209962A81003B11C8FA58EF56329'
+  # checksumType64= 'sha256' # Default is checksumType
+}
+Install-ChocolateyZipPackage @originalGameArgs
+
+# Rename extracted directory
+$extractedDirectory = Join-Path $installationDirPath -ChildPath '3D Pinball x64'
+$installationDirName = Split-Path "$installationDirPath" -Leaf
+Write-Output "Renaming '$extractedDirectory' to '$installationDirName'..."
+Pause
+Rename-Item "$extractedDirectory" -NewName "$installationDirName"
+Write-Debug "Renamed '$extractedDirectory' to '$installationDirName'."
+Pause
+
+# Prevent incorrect shimming
+# by creating a '.ignore' file and a '.gui' file
+$executableDirPath = Join-Path "$installationDirPath" # Also used for shortcuts
+$dotIgnorePath = Join-Path "$executableDirPath" -ChildPath 'PINBALL.exe.ignore'
+$dotGuiPath = Join-Path "$executableDirPath" -ChildPath 'SpaceCadetPinball.exe.gui'
+Write-Verbose "Creating file '$dotIgnorePath'..."
+New-Item -Path "$dotIgnorePath" -ItemType File > $null
+Write-Debug "File '$dotIgnorePath' created."
+Write-Verbose "Creating file '$dotGuiPath'..."
+New-Item -Path $dotGuiPath -ItemType File > $null
+Write-Debug "File '$dotGuiPath' created."
+
+Pause
+
 # Extract archive
 # Documantation - https://docs.chocolatey.org/en-us/create/functions/get-chocolateyunzip
 # Source code - https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/helpers/functions/Get-ChocolateyUnzip.ps1
 # Paths
 # In Chocolatey scripts, ALWAYS use absolute paths
-$toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$zipArchivePath = Join-Path $toolsDirPath -ChildPath 'NAME_OF_EMBEDDED_ZIP_FILE.zip'
-$installationDirPath = $toolsDirPath
+$zipArchivePath = Join-Path $toolsDirPath -ChildPath 'SpaceCadetPinballx86Win.zip'
+$zipArchive64Path = Join-Path $toolsDirPath -ChildPath 'SpaceCadetPinballx64Win.zip'
 # Arguments
 $unzipArgs = @{
-  PackageName  = "$($packageName)"
-  FileFullPath = "$zipArchivePath"
-  Destination  = "$installationDirPath"
+  PackageName    = "$($packageName)"
+  FileFullPath   = "$zipArchivePath"
+  FileFullPath64 = "$zipArchive64Path"
+  Destination    = "$installationDirPath"
 }
 # Unzip file to the specified location - auto overwrites existing content
 Get-ChocolateyUnzip @unzipArgs
@@ -68,14 +112,13 @@ Get-ChocolateyUnzip @unzipArgs
 ## Documantation - https://docs.chocolatey.org/en-us/create/functions
 ## There may also be functions available in extension packages
 ## See here for examples and availability: https://community.chocolatey.org/packages?q=id%3A.extension
-
+<# 
 # Add shortcuts
 # Documantation - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyshortcut
 # Source code - https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/helpers/functions/Install-ChocolateyShortcut.ps1
 if ($addDesktopShortcut -or $addStartMenuShortcut) {
   # Paths
-  $executableDirPath = $toolsDirPath
-  $executablePath = Join-Path $executableDirPath "$($packageName).exe"
+  $executablePath = Join-Path "$executableDirPath" -ChildPath 'PINBALL.exe.ignore'
   $desktopShortcutPath = "$env:UserProfile\Desktop\$shortcutName.lnk"
   $startMenuShortcutPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk"
   $iconPath = Join-Path "$executableDirPath" -ChildPath "$($packageName).ico"
@@ -118,3 +161,4 @@ if ($addStartMenuShortcut) {
     "$startMenuShortcutPath" | Out-File "$shortcutsLog" -Append
   }
 }
+ #>
