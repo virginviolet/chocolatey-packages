93fce1bb5b24ac8f142bdf71efc891fbd9306351
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-11-05 20:01:29 +0100

    violet-perfectionist-helpers: Modified variable name

--- extensions/violet-perfectionist-helpers.extension/extensions/violetPerfectionistHelpers.psm1
+++ extensions/violet-perfectionist-helpers.extension/extensions/violetPerfectionistHelpers.psm1
@@ -1,9 +1,9 @@
 # Export functions that start with capital letter, others are private
 # Include file names that start with capital letters, ignore others
-$ScriptRoot = Split-Path $MyInvocation.MyCommand.Definition
+$scriptRootPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $pre = Get-ChildItem Function:\*
-Get-ChildItem "$ScriptRoot\*.ps1" |
+Get-ChildItem "$scriptRootPath\*.ps1" |
     Where-Object { $_.Name -cmatch '^[A-Z]+' } |
     ForEach-Object { . $_  }
 $post = Get-ChildItem Function:\*
