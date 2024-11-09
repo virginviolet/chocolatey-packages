f47860c300075b87dfd5f9361a8777694547d8c3
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-11-09 16:37:03 +0100

    spacecadetpinball: Fixed failure when 'Games' not on Start Menu

--- manual/spacecadetpinball/tools/chocolateyinstall.ps1
+++ manual/spacecadetpinball/tools/chocolateyinstall.ps1
@@ -3,18 +3,18 @@
 # Initialization
 $ErrorActionPreference = 'Stop' # Stop on all errors
 $toolsDirPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$packagePath = "$(Split-Path -Parent $toolsDirPath)"
+$packagePath = "$(Split-Path -Parent "$toolsDirPath")"
 
 # Preferences
 $installationDirName = '3D Pinball x64'
-$installationDirPath = Join-Path "$toolsDirPath" "$installationDirName" # Used for shortcut installation
+$installationDirPath = Join-Path -Path "$toolsDirPath" -ChildPath "$installationDirName" # Used for shortcut installation
 $shortcutName = 'Pinball'
 $logShortcuts = $true
 
 # Download and unpack a zip file
 # Path
 $url = 'https://ia902303.us.archive.org/28/items/3d-pinball-x64/3D%20Pinball%20x64.zip'
-$unzipDirPath = Split-Path $installationDirPath
+$unzipDirPath = Split-Path -LiteralPath "$installationDirPath"
 # Arguments
 $originalGameArgs = @{
   packageName   = 'spacecadetpinball'
@@ -27,9 +27,9 @@ Install-ChocolateyZipPackage @originalGameArgs
 
 # Extract decompilation release archive
 # Paths
-$zipArchiveX64Path = Join-Path $toolsDirPath -ChildPath 'SpaceCadetPinballx64Win.zip'
-$zipArchiveX86Path = Join-Path $toolsDirPath -ChildPath 'SpaceCadetPinballx86Win.zip'
-$zipArchiveX86XpPath = Join-Path $toolsDirPath -ChildPath 'SpaceCadetPinballx86WinXP.zip'
+$zipArchiveX64Path = Join-Path -Path "$toolsDirPath" -ChildPath 'SpaceCadetPinballx64Win.zip'
+$zipArchiveX86Path = Join-Path -Path "$toolsDirPath" -ChildPath 'SpaceCadetPinballx86Win.zip'
+$zipArchiveX86XpPath = Join-Path -Path "$toolsDirPath" -ChildPath 'SpaceCadetPinballx86WinXP.zip'
 # Select which of x86 archives to use
 $userOs = $([System.Environment]::OSVersion).Platform
 # Windows NT (or Win32NT) is Windows XP up to Windows 11 (as of typing)
@@ -53,7 +53,7 @@ if ($userOsIsXp) {
   $zipArchiveX86CorrectPath = $zipArchiveX86Path
 }
 # Package name
-$packageName = Split-Path $packagePath -Leaf
+$packageName = Split-Path -Path "$packagePath" -Leaf
 # Arguments
 $decompiledArgs = @{
   PackageName    = "$packageName"
@@ -66,22 +66,23 @@ Get-ChocolateyUnzip @decompiledArgs
 
 # Add shortcuts
 # Paths
-$desktopPath = Convert-Path "$env:UserProfile\Desktop\"
-$desktopShortcutPath = Join-Path "$desktopPath" -ChildPath "$shortcutName.lnk"
+$desktopPath = Convert-Path -LiteralPath "$env:UserProfile\Desktop\"
+$desktopShortcutPath = Join-Path -Path "$desktopPath" -ChildPath "$shortcutName.lnk"
 $isElevated = Test-ProcessAdminRights
 if ($isElevated) {
-  # Make Startu Menu shortcut available to all users
-  $startMenuPath = Convert-Path "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\Games\"
+  # Make Start Menu shortcut available to all users
+  $startMenuPath = Convert-Path -LiteralPath "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
 } else {
-  # Make Startu Menu shortcut available to current user only
-  $startMenuPath = Convert-Path "$Env:AppData\Microsoft\Windows\Start Menu\Programs\Games\"
+  # Make Start Menu shortcut available to current user only
+  $startMenuPath = Convert-Path -LiteralPath "$Env:AppData\Microsoft\Windows\Start Menu\Programs\"
 }
-$startMenuShortcutPath = Join-Path "$startMenuPath" -ChildPath "$shortcutName.lnk"
-$gameDirShortcutPath = Join-Path "$installationDirPath" -ChildPath "Launch $shortcutName.lnk"
+$startMenuGamesPath = Join-Path -Path "$startMenuPath" -ChildPath "Games"
+$startMenuShortcutPath = Join-Path -Path "$startMenuGamesPath" -ChildPath "$shortcutName.lnk"
+$gameDirShortcutPath = Join-Path -Path "$installationDirPath" -ChildPath "Launch $shortcutName.lnk"
 $executableDirPath = $installationDirPath
-$executablePath = Join-Path "$installationDirPath" -ChildPath 'SpaceCadetPinball.exe'
+$executablePath = Join-Path -Path "$installationDirPath" -ChildPath 'SpaceCadetPinball.exe'
 if ($logShortcuts) {
-  $shortcutsLogPath = Join-Path "$packagePath" -ChildPath "shortcuts.txt"
+  $shortcutsLogPath = Join-Path -Path "$packagePath" -ChildPath "shortcuts.txt"
 }
 # Description
 $shortcutDescription = "Begins a game of 3-D Pinball."
@@ -91,7 +92,7 @@ Install-ChocolateyShortcut -shortcutFilePath "$desktopShortcutPath" `
   -workingDirectory "$executableDirPath" `
   -description $shortcutDescription
 if ($logShortcuts) {
-  "$desktopShortcutPath" | Out-File "$shortcutsLogPath" -Append
+  "$desktopShortcutPath" | Out-File -FilePath "$shortcutsLogPath" -Append
 }
 # Add start menu shortcut
 Install-ChocolateyShortcut -shortcutFilePath "$startMenuShortcutPath" `
@@ -104,5 +105,5 @@ if ($logShortcuts) {
 # Add game directory shortcut
 Install-ChocolateyShortcut -shortcutFilePath "$gameDirShortcutPath" -targetPath "$executablePath" -workingDirectory "$executableDirPath" -description $shortcutDescription
 if ($logShortcuts) {
-  "$gameDirShortcutPath" | Out-File "$shortcutsLogPath" -Append
+  "$gameDirShortcutPath" | Out-File -FilePath "$shortcutsLogPath" -Append
 }
