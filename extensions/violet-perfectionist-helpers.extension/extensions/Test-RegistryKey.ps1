e6141558eba47123efa1f319480198e0902da2aa
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-11-10 02:15:10 +0100

    Test-RegistryKey: Support testing for no values as well as empty key

--- extensions/violet-perfectionist-helpers.extension/extensions/Test-RegistryKey.ps1
+++ extensions/violet-perfectionist-helpers.extension/extensions/Test-RegistryKey.ps1
@@ -2,37 +2,118 @@ function Test-RegistryKey {
   param (
     [Alias("KeyPath")]
     [Alias("Key")]
-    [parameter(Mandatory = $true, Position = 0, ParameterSetName="Path")]
-    [parameter(Mandatory = $true, Position = 0, ParameterSetName="Name")]
-    [parameter(Mandatory = $true, Position = 0, ParameterSetName="Data")]
+    [parameter(Mandatory = $true, Position = 0, ParameterSetName = "Path")]
+    [parameter(Mandatory = $true, Position = 0, ParameterSetName = "Name")]
+    [parameter(Mandatory = $true, Position = 0, ParameterSetName = "Data")]
     [string]
     $Path,
     
     [Alias("ValueName")]
-    [parameter(Mandatory = $true, Position = 1, ParameterSetName="Name")]
-    [parameter(Mandatory = $true, Position = 1, ParameterSetName="Data")]
+    [parameter(Mandatory = $true, Position = 1, ParameterSetName = "Name")]
+    [parameter(Mandatory = $true, Position = 1, ParameterSetName = "Data")]
     [string]$Name,
     
-    [Alias("ValueData","Data")]
-    [parameter(Mandatory = $true, Position = 2, ParameterSetName="Data")]
+    [Alias("ValueData", "Data")]
+    [parameter(Mandatory = $true, Position = 2, ParameterSetName = "Data")]
     $Value
   )
+
   if (-not $Path.StartsWith("REGISTRY::") -and `
-      $Path -ne '' -and `
-      $null -ne $Path) {
-        $Path = $Path.Insert(0,"REGISTRY::")
+  $Path -ne '' -and `
+  $null -ne $Path) {
+    $pathFriendly = $Path
+    $Path = $Path.Insert(0, "REGISTRY::")
+  } else {
+    $pathFriendly = $Path.Replace("REGISTRY::", "")
   }
-  try {
-    # Write-Debug "Testing if the registry key '$Path' exists..."
-    Get-ItemProperty -Path $Path -ErrorAction Stop > $null
-    if ($Name) {
-      # Write-Debug "Testing if registry key '$Path' has the value '$Name'..."
-      Get-ItemProperty -Path $Path -Name $Name -ErrorAction Stop > $null
-      # Write-Debug "Value found."
+
+  # Calls to these functions need to be placed inside a try-catch block,
+  # Otherwise, they aren't really testing anything.
+
+  function Test-Values($GetValuePath) {
+    $values = $(Get-RegistryKey -Path $GetValuePath -ErrorAction Stop)
+    if ($null -ne $values) {
+      $valueCount = $values.Length
+      if ($null -ne $valueCount) {
+        Write-Debug "$valueCount values found."
+      } else {
+        Write-Debug "Value found."
+      }
+      return $true
+    } else {
+      return $false
     }
+  }
+
+  try {
+    Write-Debug "Validating registry key path '$pathFriendly'..."
+    # Look for key(s)
+    if (-Not $Name) {
+      # If 'Path' ends with '*\',
+      # then '$key' will hold child keys (think "directories"),
+      # otherwise, '$key' will hold values (think "files")
+      if (-not $Path.EndsWith('\*') -and -not $Path.EndsWith('/*')) {
+        # Look for a single key
+        Write-Debug "Looking for key '$pathFriendly'..."
+        $values = $(Get-RegistryKey -Path $Path -ErrorAction Stop)
+        Write-Debug "Key found."
+        return $true
+      } else {
+        # Look for child keys
+        # Get the parent key by removing the asterisk
+        $parentKeyPath = $Path.Substring(0, $Path.Length - 1)
+        $parentKeyPathFriendly = $parentKeyPath.Replace("REGISTRY::", "")
+        Write-Debug "Looking for key '$parentKeyPathFriendly'..."
+        # Look for child keys and values 
+        $childKeys = $(Get-ChildItem -Path $Path -ErrorAction Stop)
+        Write-Debug "Key found."
+        if ($null -ne $childKeys) {
+          Write-Debug "Child keys found."
+          return $true
+        } else {
+          Write-Debug "No child key(s) found."
+          Write-Debug "Looking for values in key '$parentKeyPathFriendly'..."
+          $valuesExist = Test-Values -GetValuePath $parentKeyPath
+          if ($valuesExist) {
+            return $true
+          } else {
+            Write-Debug "No values found."
+            return $false
+          }
+        }
+      }
+    }
+
+    # Look for value(s)
+    if (-not $Value) {
+      # Get-RegistryKey -Path $Path -ErrorAction Stop > $null
+      if ($Name -ne '*' -and $Name -ne '*') {
+        if ($Path.Contains('*')) {
+          $message = "Matching part of a value name is not supported. " + `
+          "You can, however, search for all values in a key with '-Name *'"
+          Write-Warning $message
+        }
+        # Look for a single value
+        Write-Debug "Testing if registry key '$pathFriendly' exists and if it has the value '$Name'..."
+        $value = $(Get-RegistryKey -Path $Path -Name $Name -ErrorAction Stop)
+        Write-Debug "Key exists and value found."
+        return $true
+      } else {
+        # Look for multiple values
+        $valuesExist = Test-Values -GetValuePath $Path
+        if ($valuesExist) {
+          return $true
+        } else {
+          Write-Debug "Value not found."
+          return $false
+        }
+      }
+    }
+
+    # Look for value data
     if ($Value) {
-      # Write-Debug "Testing if the value '$name' has the data '$Value'..."
-      $existingValueData = Get-RegistryKey -Path $Path -Name $Name -ErrorAction Stop
+      Write-Debug "Testing if the key '$pathFriendly' exists, if it has a value named '$Name', and if the value contains the data '$Value'..."
+      $existingValueData = $(Get-RegistryKey -Path $Path -Name $Name -ErrorAction Stop)
       # Write-Debug "Value data matches the input."
       if ($existingValueData -eq $Value) {
         return $true
@@ -40,14 +121,14 @@ function Test-RegistryKey {
         return $false
       }
     }
-    return $true
+
   } catch [System.Management.Automation.ItemNotFoundException] {
-    <# $message = "Could not get registry key value data.`n" + `
+    <# $message = "Could not validate registry key path.`n" + `
       "The key '$Path' does not exist.`n$_"
     Write-Debug $message #>
     return $false
   } catch [System.Management.Automation.PSArgumentException] {
-    <# $message = "Could not get registry key value data. `n" + 
+    <# $message = "Could not validate registry key path.`n" + 
       "The key '$Path' does not have the value '$Name'.`n$_"
     Write-Debug $message #>
     return $false
