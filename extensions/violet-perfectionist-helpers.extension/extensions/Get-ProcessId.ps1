bdbb6c3e0dfc515d65ec23887d56427ef2e985ad
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-11-15 15:56:22 +0100

    Get-ProcessId: Made it possible to use both parameters in one command

--- extensions/violet-perfectionist-helpers.extension/extensions/Get-ProcessId.ps1
+++ extensions/violet-perfectionist-helpers.extension/extensions/Get-ProcessId.ps1
@@ -1,11 +1,11 @@
 function Get-ProcessId {
   <#
     .SYNOPSIS
-    Retrieves the ID of each process that have a certain name or command line property value.
+    Retrieves the ID of each process that have a certain name and/or command line property value.
 
     .DESCRIPTION
-    The Get-ProcessId function retrieves the IDs of processes by either its command line property
-    (the full command line string that was used to start the process) or name.  
+    The Get-ProcessId function retrieves the IDs of processes by its command line property
+    (the full command line string that was used to start the process) and/or name.  
     The function does not output anything to the console by itself.
 
     .PARAMETER Name
@@ -16,43 +16,82 @@ function Get-ProcessId {
     Mandatory if Name is not specified.  
     Specifies the whole or part of a command line property value (the full command line string that
     was used to start the process) to filter for.  
+    
+    .EXAMPLE
+    $autoHotkeyProcessId = Get-ProcessId -CommandLine 'C:\My Scripts\hideWindow.ahk'
+    Retrieves the process IDs of all processes whose command line property value includes the string
+    `C:\My Scripts\hideWindow.ahk`.
 
     .EXAMPLE
     $notepadProcessId = Get-ProcessId -Name 'notepad.exe'
     Retrieves the process IDs of all instances of Notepad.
 
     .EXAMPLE
-    $autoHotkeyProcessId = Get-ProcessId -CommandLine 'C:\My Scripts\hideWindow.ahk'
-  
-    Retrieves the process IDs of all processes whose command line property value includes the string `C:\My Scripts\hideWindow.ahk`.
+    $notepadProcessId = Get-ProcessId -Name 'notepad.exe' -CommandLine 'C:\My Scripts\hideWindow.ahk'
+    Retrieves the process IDs of all processes whose name is `notepad.exe` and whose command line
+    property value includes the string `C:\My Scripts\hideWindow.ahk`.
 
     .NOTES
-    This function uses the Get-CimInstance cmdlet to query the Win32_Process class for process information.
+    - This function uses the Get-CimInstance cmdlet to query the Win32_Process class for process
+      information.
+    - No error will occur if no processes are found.
   #>
+
   param (
     [parameter(Mandatory = $true, Position = 0, ParameterSetName = "Name")]
+    [parameter(Mandatory = $true, Position = 0, ParameterSetName = "NameAndCommandLine")]
     [string]
     $Name,
+
     [parameter(Mandatory = $true, Position = 0, ParameterSetName = "CommandLine")]
+    [parameter(Mandatory = $true, Position = 1, ParameterSetName = "NameAndCommandLine")]
     [string]
     $CommandLine
   )
-  if ($Name) {
-    $processIds = Get-CimInstance Win32_Process | Where-Object {
-      $_.Name -like $Name
-    } | ForEach-Object {
-      Write-Output "$($_.ProcessId)"
-    }
-  } elseif ($CommandLine) {
-    # Escape characters for the path (by no means complete)
-    $commandLineEscaped = $CommandLine -replace '\\', '\\' `
+
+  function ConvertTo-EscapedString {
+    <#
+      .SYNOPSIS
+      Converts a string to an escaped string.
+
+      .PARAMETER String
+      The string to be escaped.
+
+      .NOTES
+      This function currently escapes the following characters: backslash (`\`), colon (`:`), and
+      parentheses (`(` and `)`).
+    #>
+    param (
+      [ValidateNotNullOrEmpty()]
+      [parameter(Mandatory = $true, Position = 0)]
+      [string]
+      $String
+    )
+    $escapedString = $String -replace '\\', '\\' `
       -replace ':', '\:' -replace '\(', '\(' -replace '\)', '\)'
-    $processIds = Get-CimInstance Win32_Process | Where-Object {
-      $_.CommandLine -match $commandLineEscaped
-    } | ForEach-Object {
-      Write-Output "$($_.ProcessId)"
-    }
+    return $escapedString
   }
+
+  if ($CommandLine) {
+    $commandLineEscaped = ConvertTo-EscapedString -String $CommandLine
+  }
+
+  if ($Name -and $CommandLine) {
+    $processIds = Get-CimInstance Win32_Process |
+      Where-Object { $_.Name -like $Name -and $_.CommandLine -match $commandLineEscaped } |
+      ForEach-Object { Write-Output "$($_.ProcessId)" }
+
+  } elseif ($CommandLine) {
+    $processIds = Get-CimInstance Win32_Process |
+      Where-Object { $_.CommandLine -match $commandLineEscaped } |
+      ForEach-Object { Write-Output "$($_.ProcessId)" }
+
+  } elseif ($Name) {
+    $processIds = Get-CimInstance Win32_Process |
+      Where-Object { $_.Name -like $Name } |
+      ForEach-Object { Write-Output "$($_.ProcessId)" }
+  }
+
   # Return an object containing one or many strings
   return $processIds
 }
